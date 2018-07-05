64e68a1001c841b7c5adecbc3ba7d91225f6f749
Oleksii <oleksii@MacBook-Pro-Oleksii.local>
2018-07-05 16:44:37 +0300

    Test Commit (:

---
+++ History of Weather Trends/TestC.swift
@@ -0,0 +1,13 @@
+//
+//  TestC.swift
+//  History of Weather Trends
+//
+//  Created by Oleksii on 05.07.18.
+//  Copyright © 2018 Oleksii. All rights reserved.
+//
+
+import Foundation
+
+enum Test {
+    case asd, dsa
+}
